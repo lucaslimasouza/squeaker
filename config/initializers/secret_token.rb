f90c34c99ef9452497bf654c59fce3af6d14fa4d
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-10-24 22:52:41 -0200

    defining first scenario

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squeaker::Application.config.secret_token = 'f679b56057d36fea3760ec45537f262b5258499e55d2c76e7979b6cab4b42c21b21974a1127d4b74b88c6fa0e5831497e3a2b8be8b4a8ab63c843dea3abf481d'
